8866741b1756099a04817c62b048985ecc8a6e0e
hkxnyc <hannah.kim@flatironschool.com>
2015-11-02 21:27:17 -0500

    objc-speaking-grandma

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -13,6 +13,59 @@
      
      */
     
+    //CODE-ALONG 1: TALK TO GRANDMA
+    //1
+    NSString*talkToGrandma = @"Hi, Grandma!";
+    //2
+    NSString*shoutAtGrandma = [talkToGrandma uppercaseString];
+
+    //6
+    talkToGrandma = [talkToGrandma uppercaseString];
+    
+    //3
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    //4
+    if (shouting) {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    //5
+    else{
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    
+    //CODE-ALONG 2: INVERTED CHECK
+    //1
+    if (![talkToGrandma isEqualToString: shoutAtGrandma]) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    } else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    
+    //ADVANCED
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return YES; // Don't alter this statement.
 }
 
